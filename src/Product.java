d379d1ebbb9857585d5a84c04baf1c9700748592
Ziqi Feng <ziqifeng@Ziqis-MacBook-Pro.local>
2017-03-16 12:52:02 -0400

    Pass new Junit test for Material part
    Since enum Material is moved into Product Class, new JUnit tests are
    created and all of them passed.

--- src/Product.java
+++ src/Product.java
@@ -1,7 +1,6 @@
 /**
  *
  * This class will hold all information about a product object including basePrice, #person need, and material type etc
- * 
  * @author ziqifeng
  *
  */
@@ -11,14 +10,13 @@ public class Product {
 	 * This enum Material contains the name of the material type 
 	 * the index of the type will be used as numeric codes to allow user to assign the associated material to product
 	 * putting material as a inner enum because it will be access only by Product class
-	 * most importantly, there is a private value which we dont want to access by others associated to each Constant 
-	 * @author ziqifeng
+	 * most importantly, there is a protect value associated to each Constant and we dont want others but Product Object to access them
 	 *
 	 */
 	public enum Material {
 			PHARMACEUTICALS(0.075), FOOD(0.13), ELECTRONIC(0.02), OTHERS(0);
 			
-			private double rate;
+			protected double rate;
 
 			private Material(double value) {
 				this.rate = value;
@@ -32,11 +30,11 @@ public class Product {
 	private Product.Material type;
 	
 	
-	
 	public Product(double basePrice, int numPerson, Product.Material type) {
 		this.basePrice = basePrice;
 		this.numPerson = numPerson;
 		this.type = type;		
+		
 	}
 	
 	public double getPrice() {
--- src/ProductTest.java
+++ src/ProductTest.java
@@ -17,6 +17,21 @@ public class ProductTest {
 	}	
 	
 	@Test
+	public void testMaterialRate() {
+		Product.Material type=Product.Material.FOOD;
+		assertEquals(0.13,type.rate,0);
+	}
+	
+	public void testMaterialCode(){
+
+		 // The numeric material type code is associated to the index of Material
+		assertEquals(Product.Material.values()[0],Product.Material.PHARMACEUTICALS);
+		assertEquals(Product.Material.values()[1],Product.Material.FOOD);
+		assertEquals(Product.Material.values()[2],Product.Material.ELECTRONIC);
+		assertEquals(Product.Material.values()[3],Product.Material.OTHERS);
+	}
+	
+	@Test
 	public void testMaterial() {
 		Product product=new Product(1299.99,3,Product.Material.FOOD);
 		assertEquals(Product.Material.FOOD,product.getMaterial());
